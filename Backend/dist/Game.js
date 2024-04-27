9fb32a41d236b2a909062116c3542220d6fdf427
Prem R Pattanayak <prem.p.in@rupeek.com>
2024-04-27 13:23:41 +0530

    added working backend

--- Backend/dist/Game.js
+++ Backend/dist/Game.js
@@ -1,84 +1,74 @@
-import { WebSocket } from "ws";
-import { Chess } from 'chess.js'
-import { GAME_OVER, INIT_GAME, MOVE } from "./messages";
-
-export class Game {
-    public player1: WebSocket;
-    public player2: WebSocket;
-    private board: Chess;
-    private startTime: Date;
-    private moveCount = 0;
-
-    constructor(player1: WebSocket, player2: WebSocket){
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Game = void 0;
+const chess_js_1 = require("chess.js");
+const messages_1 = require("./messages");
+class Game {
+    constructor(player1, player2) {
+        this.moveCount = 0;
         this.player1 = player1;
         this.player2 = player2;
-        this.board = new Chess();
+        this.board = new chess_js_1.Chess();
         this.startTime = new Date();
-
         this.player1.send(JSON.stringify({
-            type: INIT_GAME,
+            type: messages_1.INIT_GAME,
             payload: {
                 color: 'white'
             }
-        }))
-
+        }));
         this.player2.send(JSON.stringify({
-            type: INIT_GAME,
+            type: messages_1.INIT_GAME,
             payload: {
                 color: 'black'
             }
-        }))
+        }));
     }
-
-    makeMove(socket: WebSocket, move: {
-        from: string,
-        to: string
-    }){
+    makeMove(socket, move) {
         // validate game moves
-        if(this.moveCount === 0 && socket !== this.player1){
+        if (this.moveCount === 0 && socket !== this.player1) {
             return;
         }
-        if(this.moveCount % 2 === 1 && socket !== this.player2){
+        if (this.moveCount % 2 === 1 && socket !== this.player2) {
             return;
         }
-
         console.log(move);
         try {
             this.board.move(move);
-        } catch (error) {
+        }
+        catch (error) {
             console.error(error);
             return;
         }
-        
         // handle game over and winner logic
-        if(this.board.isGameOver()){
+        if (this.board.isGameOver()) {
             this.player1.send(JSON.stringify({
-                type: GAME_OVER,
+                type: messages_1.GAME_OVER,
                 payload: {
                     winner: this.board.turn() === 'w' ? 'black' : 'white'
                 }
-            }))
+            }));
             this.player2.send(JSON.stringify({
-                type: GAME_OVER,
+                type: messages_1.GAME_OVER,
                 payload: {
                     winner: this.board.turn() === 'w' ? 'black' : 'white'
                 }
-            }))
+            }));
             return;
         }
-
         // notify players that move has been made
-        if(this.moveCount % 2 === 0){
+        if (this.moveCount % 2 === 0) {
             this.player2.send(JSON.stringify({
-                type: MOVE,
+                type: messages_1.MOVE,
                 payload: move
-            }))
-        } else {
+            }));
+        }
+        else {
             this.player1.send(JSON.stringify({
-                type: MOVE,
+                type: messages_1.MOVE,
                 payload: move
-            }))
+            }));
         }
         this.moveCount++;
     }
 }
+exports.Game = Game;
